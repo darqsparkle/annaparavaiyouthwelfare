a8afa4f7b09005792a71f3877fd5e1a4ee5b3e55
darqsparkle <roovis2003@gmail.com>
2026-07-26 21:47:42 +0530

    chang

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
-      <Founders />
+      {/* <Founders /> */}
       <ExecutiveCommittee />
       <Stats />
       <GalleryPreview />
